95087fc5ded4869112a1630f3370d04da4b87d11
Meta Vens <rwvens@hotmail.com>
2026-05-23 20:18:13 +0100

    Ship committed-focus article geometry

--- site/quartz/components/ZyphosGraphShell.tsx
+++ site/quartz/components/ZyphosGraphShell.tsx
@@ -3,9 +3,9 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 export default (() => {
   const ZyphosGraphShell: QuartzComponent = () => (
     <>
-      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css?v=graph-20260523-focus3" />
+      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css?v=graph-20260523-focus6" />
       <div class="zyphos-epiphany-graph-root"></div>
-      <script type="module" src="/static/epiphany-graph/assets/viewer.js?v=graph-20260523-focus3"></script>
+      <script type="module" src="/static/epiphany-graph/assets/viewer.js?v=graph-20260523-focus6"></script>
     </>
   )
 
