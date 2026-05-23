c2b4f3972f8270234f1ab8ce60347c9c72ce9a98
Meta Vens <rwvens@hotmail.com>
2026-05-23 22:52:19 +0100

    Ship continuous viewport focus reconciliation

--- site/quartz/components/ZyphosGraphShell.tsx
+++ site/quartz/components/ZyphosGraphShell.tsx
@@ -3,9 +3,9 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 export default (() => {
   const ZyphosGraphShell: QuartzComponent = () => (
     <>
-      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css?v=graph-20260523-focus7" />
+      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css?v=graph-20260523-focus8" />
       <div class="zyphos-epiphany-graph-root"></div>
-      <script type="module" src="/static/epiphany-graph/assets/viewer.js?v=graph-20260523-focus7"></script>
+      <script type="module" src="/static/epiphany-graph/assets/viewer.js?v=graph-20260523-focus8"></script>
     </>
   )
 
