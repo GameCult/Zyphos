9a22636ce8fc694eab3f14d0da6d6039853d9170
Meta Vens <rwvens@hotmail.com>
2026-05-23 19:10:00 +0100

    Restore graph shell asset loading

--- site/quartz/components/ZyphosGraphShell.tsx
+++ site/quartz/components/ZyphosGraphShell.tsx
@@ -3,9 +3,9 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 export default (() => {
   const ZyphosGraphShell: QuartzComponent = () => (
     <>
-      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css" />
+      <link rel="stylesheet" href="/static/epiphany-graph/assets/viewer.css?v=graph-20260523-atomic" />
       <div class="zyphos-epiphany-graph-root"></div>
-      <script type="module" src="/static/epiphany-graph/assets/viewer.js"></script>
+      <script type="module" src="/static/epiphany-graph/assets/viewer.js?v=graph-20260523-atomic"></script>
     </>
   )
 
